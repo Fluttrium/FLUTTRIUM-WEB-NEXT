3918f32daf8586f219e0fd44ab2e880e2d95c5f1
Franz <f.o.larionov@gmail.com>
2024-08-11 13:19:10 +0300

    поменял фотки и текст

--- src/components/Contacs.tsx
+++ src/components/Contacs.tsx
@@ -6,19 +6,19 @@ import { FaXTwitter } from 'react-icons/fa6';
 const SocialContacts = () => {
     return (
         <div id="contacs" className="flex flex-wrap justify-center gap-5 p-5 bg-gray-100 rounded-lg md:gap-10 md:p-10">
-            <IconButton href="" target="_blank" rel="noopener noreferrer" aria-label="Telegram">
+            <IconButton href="https://t.me/fluttrium_official" target="_blank" rel="noopener noreferrer" aria-label="Telegram">
                 <FaTelegramPlane className="text-black text-4xl md:text-6xl" />
             </IconButton>
-            <IconButton href="" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
+            <IconButton href="https://www.tiktok.com/@fluttrium" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
                 <FaTiktok className="text-black text-4xl md:text-6xl" />
             </IconButton>
-            <IconButton href="" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+            <IconButton href="https://github.com/orgs/Fluttrium" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
                 <FaGithub className="text-black text-4xl md:text-6xl" />
             </IconButton>
             <IconButton href="" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
                 <FaInstagram className="text-black text-4xl md:text-6xl" />
             </IconButton>
-            <IconButton href="" target="_blank" rel="noopener noreferrer" aria-label="Youtube">
+            <IconButton href="https://www.youtube.com/channel/UCcBh05h-yxZOzXhnOz9ub5w" target="_blank" rel="noopener noreferrer" aria-label="Youtube">
                 <FaYoutube className="text-black text-4xl md:text-6xl" />
             </IconButton>
         </div>

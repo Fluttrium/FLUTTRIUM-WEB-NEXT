56d4a3054446c8cd8e3cdc0afc9a6c7fc1a9407e
romanmaksimov <maximov.roman@outlook.com>
2024-11-18 21:18:27 +0100

    создал обработку форму на гл эк

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,46 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 import { cn } from "@/lib/utils";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { useTranslations } from "use-intl";
 
 export function Form() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [description, setDescription] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Form submitted");
+    setLoading(true);
+
+
+    try {
+      const response = await fetch('/api/form', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name, email, phone, description}),
+      });
+
+      if (response.ok) {
+
+        setName('');
+        setEmail('');
+        setPhone('');
+        setDescription('');
+      } else {
+        const errorData = await response.json();
+
+      }
+    } finally {
+      setLoading(false);
+    }
   };
   const t = useTranslations("MainForm");
 
@@ -22,31 +53,25 @@ export function Form() {
       </div>
       <div className="md:w-1/2">
         <div className="max-w-md w-full mx-auto rounded-none p-4 bg-white dark:bg-black my-2">
-          <form className="space-y-4" onSubmit={handleSubmit}>
+          <form className="space-y-11" onSubmit={handleSubmit}>
             <div className="flex flex-col space-y-2 mb-4">
               <LabelInputContainer>
                 <Label htmlFor="firstname">{t("label1")}</Label>
                 <Input
+                    onChange={(e) => setName(e.target.value)}
                   id="firstname"
                   placeholder="Tyler"
                   type="text"
                   className="h-8 text-sm"
                 />
               </LabelInputContainer>
-              <LabelInputContainer>
-                <Label htmlFor="lastname">{t("label2")}</Label>
-                <Input
-                  id="lastname"
-                  placeholder="Durden"
-                  type="text"
-                  className="h-8 text-sm"
-                />
-              </LabelInputContainer>
+
             </div>
             {/* Поле email отображается только на десктопе, Рамзес сделай поле не обязательным */}
-            <LabelInputContainer className="hidden md:block mb-4">
+            <LabelInputContainer className="mb-4">
               <Label htmlFor="email">{t("label3")}</Label>
               <Input
+                  onChange={(e) => setEmail(e.target.value)}
                 id="email"
                 placeholder="example@.com"
                 type="email"
@@ -56,6 +81,7 @@ export function Form() {
             <LabelInputContainer className="mb-4">
               <Label htmlFor="phone">{t("label4")}</Label>
               <Input
+                  onChange={(e) => setPhone(e.target.value)}
                 id="phone"
                 placeholder="+7 921 457 00 57"
                 type="phone"
@@ -64,11 +90,11 @@ export function Form() {
             </LabelInputContainer>
             <LabelInputContainer className="mb-4">
               <Label htmlFor="idea">{t("label5")}</Label>
-              <textarea
+              <Input onChange={(e) => setDescription(e.target.value)}
                 id="idea"
                 placeholder={t("placeHold")}
                 className="border border-gray-300 rounded-md p-2 w-full h-20 resize-none text-sm"
-              ></textarea>
+              ></Input>
             </LabelInputContainer>
             <button
               className="bg-gradient-to-br from-black dark:from-zinc-900 to-neutral-600 dark:to-zinc-900 w-full text-white rounded-md h-10 text-sm font-medium"
